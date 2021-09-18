b8456c2112e401a876e6505059120f3ece5f4456
李国冬 <liguodongiot@163.com>
2021-09-18 14:14:27 +0800

    fix

--- tests/handle_list.py
+++ tests/handle_list.py
@@ -10,3 +10,11 @@ result.extend(temp)
 print(result)
 
 
+dirs = "file:///Users/liguodong/work/data/temp/classify/one, file:///Users/liguodong/work/data/temp/classify/two ,file:///Users/liguodong/work/data/temp/classify/three,/Users/liguodong/work/data/temp/classify/two"
+dirs = dirs.replace("file://", "")
+dir_list = dirs.split(',')
+dir_list = [ dir.strip() for dir in dir_list ]
+print(dir_list)
+
+
+
